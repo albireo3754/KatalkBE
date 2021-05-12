9b85cac53b3d6d2c2672c802cc22e24f3d05cc75
starpro123 <starpro123@naver.com>
2021-05-12 21:20:10 +0900

    feature: Get puuid acountid api

--- src/libs/Crawl.ts
+++ src/libs/Crawl.ts
@@ -60,6 +60,9 @@ function readCSVproID(url: string) {
 
 function saveJSON(data: unknown, url: string) {
   const json = JSON.stringify(data);
+  if (!url.match(/\.json$/)) {
+    url += '.json';
+  }
   fs.writeFileSync(url, json);
 }
 
@@ -79,8 +82,7 @@ function isActivatedID(LP: string) {
   return false;
 }
 
-async function getSummonerV4(summonerName: string) {
-  // summonerName : [KR] 이름~
+function getNameAndCountry(summonerName: string) {
   const [rawCountry, rawName] = summonerName.split('] ');
   const name = encodeURIComponent(rawName);
   let country;
@@ -89,38 +91,84 @@ async function getSummonerV4(summonerName: string) {
   } else {
     country = rawCountry.slice(1).toLowerCase() + '1';
   }
-  console.log(
-    `https://${country}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${process.env.RIOT}`,
-  );
+  return [name, country];
+}
+
+async function getSummonerV4({ country, name }: { country: string; name: string }) {
+  // summonerName : [KR] 이름~
   return await axios.get(
     `https://${country}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${process.env.RIOT}`,
   );
 }
 
-async function getSummonerV4test() {
-  console.log(await getSummonerV4('[EUW] DWG KIA'));
-  console.log(await getSummonerV4('[KR] 쭌 베'));
+// get Api 테스트
+// async function getSummonerV4test() {
+//   console.log(await getSummonerV4('[EUW] DWG KIA'));
+//   console.log(await getSummonerV4('[KR] 쭌 베'));
+// }
+// getSummonerV4test();
+
+async function sleep(ms: number) {
+  return new Promise((r) => setTimeout(r, ms));
 }
 
-getSummonerV4test();
-
-async function getProNickname() {
-  const data = await axios.get('https://www.trackingthepros.com/player/Showmaker/');
-  const root = await parse(data.data).querySelectorAll('.table')[1].querySelectorAll('td');
-  let i = 0;
-  while (i < root.length) {
-    if (i & 1) {
-      if (isActivatedID(root[i].textContent)) {
-        console.log(root[i - 1].innerText);
-        const { data } = await getSummonerV4(root[i - 1].innerText);
-        console.log(data);
+async function getProGameId(nickName: string): Promise<string[]> {
+  try {
+    const data = await axios.get(`https://www.trackingthepros.com/player/${nickName}/`);
+    const root = await parse(data.data).querySelectorAll('.table')[1].querySelectorAll('td');
+    let i = 0;
+    const nickNames: string[] = [];
+    while (i < root.length) {
+      if (i & 1) {
+        if (isActivatedID(root[i].textContent)) {
+          nickNames.push(root[i - 1].innerText);
+        }
       }
+      i++;
     }
-    i++;
+    return nickNames;
+  } catch (e) {
+    console.log(e, `${nickName} 이 안되네요`);
   }
+  return [];
   // return root.replace(/.+\s/, '');
 }
 
+async function makePuuidJSON() {
+  const raw = fs.readFileSync(path.join(__dirname, 'proID.json'));
+  const datas = JSON.parse(raw.toString()) as IProID[];
+
+  const puuidData = [];
+  for (const data of datas) {
+    if (!data.Team) {
+      continue;
+    }
+    const nickNames = await getProGameId(data.Proname);
+    for (const nickName of nickNames) {
+      try {
+        const [name, country] = getNameAndCountry(nickName);
+        await sleep(25);
+        const res = await getSummonerV4({ name, country });
+        puuidData.push({
+          proname: data.Proname,
+          position: data.Position,
+          puuid: res.data.puuid,
+          team: data.Team,
+          accountId: res.data.accountId,
+          gameId: res.data.name,
+          country: country,
+        });
+        console.log(`${nickName} 성공`);
+      } catch (e) {
+        console.log(e, 'riot API 서버에서 데이터 가져오기 실패');
+      }
+    }
+  }
+  saveJSON(puuidData, './puuid.json');
+}
+
+makePuuidJSON();
+// saveJSON({ puuid: 'hi' }, './puuid');
 // getProNickname().then((data) => {
 //   console.log(data);
 // });
